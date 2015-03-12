742902da9bfb6f1d4f52df424234ecb1f0c7acec
olov <olov.mckie@ub.uu.se>
2015-03-12 16:10:07 +0100

    EPC-61: Ändra data klasserna i The Rest
    Task-Url: http://jira.epc.ub.uu.se/browse/EPC-61

--- src/test/java/epc/therest/testdata/TestDataRecordInMemoryStorage.java
+++ src/test/java/epc/therest/testdata/TestDataRecordInMemoryStorage.java
@@ -3,18 +3,18 @@ package epc.therest.testdata;
 import java.util.HashMap;
 import java.util.Map;
 
-import epc.spider.data.SpiderDataAtomic;
-import epc.spider.data.SpiderDataGroup;
+import epc.metadataformat.data.DataAtomic;
+import epc.metadataformat.data.DataGroup;
 import epc.spider.record.storage.RecordStorageInMemory;
 
 public class TestDataRecordInMemoryStorage {
 	public static RecordStorageInMemory createRecordStorageInMemoryWithTestData() {
-		Map<String, Map<String, SpiderDataGroup>> records = new HashMap<>();
-		records.put("place", new HashMap<String, SpiderDataGroup>());
+		Map<String, Map<String, DataGroup>> records = new HashMap<>();
+		records.put("place", new HashMap<String, DataGroup>());
 
-		SpiderDataGroup recordInfo = SpiderDataGroup.withDataId("recordInfo");
-		recordInfo.addChild(SpiderDataAtomic.withDataIdAndValue("type", "place"));
-		recordInfo.addChild(SpiderDataAtomic.withDataIdAndValue("id", "place:0001"));
+		DataGroup recordInfo = DataGroup.withDataId("recordInfo");
+		recordInfo.addChild(DataAtomic.withDataIdAndValue("type", "place"));
+		recordInfo.addChild(DataAtomic.withDataIdAndValue("id", "place:0001"));
 
 		/**
 		 * <pre>
@@ -29,7 +29,7 @@ public class TestDataRecordInMemoryStorage {
 		 * </pre>
 		 */
 
-		SpiderDataGroup dataGroup = SpiderDataGroup.withDataId("authority");
+		DataGroup dataGroup = DataGroup.withDataId("authority");
 		dataGroup.addChild(recordInfo);
 
 		records.get("place").put("place:0001", dataGroup);
