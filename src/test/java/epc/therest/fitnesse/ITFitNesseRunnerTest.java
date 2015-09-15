3162c339116026f4c33db72ce380baef2a9bbf25
Uwe Klosa <uwe.klosa@ub.uu.se>
2015-09-15 15:30:05 +0200

    changed name of test suite

--- src/test/java/epc/therest/fitnesse/ITFitNesseRunnerTest.java
+++ src/test/java/epc/therest/fitnesse/ITFitNesseRunnerTest.java
@@ -1,16 +1,10 @@
 package epc.therest.fitnesse;
 
-/**
- * TODO: Class description
- *
- * @author <a href="mailto:uwe.klosa@ub.uu.se">Uwe Klosa</a>
- */
-
 import fitnesse.junit.FitNesseRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(FitNesseRunner.class)
-@FitNesseRunner.Suite("RestSuite")
+@FitNesseRunner.Suite("TheRestTests")
 @FitNesseRunner.FitnesseDir(".")
 @FitNesseRunner.OutputDir("./target/fitnesse-results")
 public class ITFitNesseRunnerTest
