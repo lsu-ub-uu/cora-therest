f8d7fa42443789c151d6eb53bd35a6aedf8f7f80
Uwe Klosa <uwe.klosa@ub.uu.se>
2015-09-15 13:22:58 +0200

    EPC-84: added integration test with junit

---
+++ src/test/java/epc/therest/fitnesse/ITFitNesseRunnerTest.java
@@ -0,0 +1,25 @@
+package epc.therest.fitnesse;
+
+/**
+ * TODO: Class description
+ *
+ * @author <a href="mailto:uwe.klosa@ub.uu.se">Uwe Klosa</a>
+ */
+
+import fitnesse.junit.FitNesseRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.Suite("RestSuite")
+@FitNesseRunner.FitnesseDir(".")
+@FitNesseRunner.OutputDir("./target/fitnesse-results")
+public class ITFitNesseRunnerTest
+{
+    @Test
+    public void dummy()
+    {
+        Assert.assertTrue(true);
+    }
+}
