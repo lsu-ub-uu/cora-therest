97a9de433f140528bed7a64e8f7fb1ff6a3720a8
support <support@ub-epc-d-adm-999>
2015-09-23 13:51:53 +0200

    EPC-82 Work on abstract recordTypes, added acceptance tests, extended Fitnesse fixture with create and readList

--- src/test/java/epc/therest/fitnesse/RecordEndpointFixture.java
+++ src/test/java/epc/therest/fitnesse/RecordEndpointFixture.java
@@ -12,10 +12,15 @@ import epc.therest.record.TestUri;
 public class RecordEndpointFixture {
 	private String id;
 	private String type;
+	private String json;
 	private StatusType statusType;
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	public void setJson(String json) {
+		this.json = json;
 	}
 
 	public void setId(String id) {
@@ -28,9 +33,31 @@ public class RecordEndpointFixture {
 
 	public String testReadRecord() {
 		UriInfo uriInfo = new TestUri();
-		SpiderInstanceProvider.setSpiderDependencyProvider(new SystemOneDependencyProvider());
+		SpiderInstanceProvider.setSpiderDependencyProvider(
+				DependencyProviderForMultipleTestsWorkingTogether.spiderDependencyProvider);
 		RecordEndpoint recordEndpoint = new RecordEndpoint(uriInfo);
 		Response response = recordEndpoint.readRecord(type, id);
+		String entity = (String) response.getEntity();
+		statusType = response.getStatusInfo();
+		return entity;
+	}
+	public String testReadRecordList() {
+		UriInfo uriInfo = new TestUri();
+		SpiderInstanceProvider.setSpiderDependencyProvider(
+				DependencyProviderForMultipleTestsWorkingTogether.spiderDependencyProvider);
+		RecordEndpoint recordEndpoint = new RecordEndpoint(uriInfo);
+		Response response = recordEndpoint.readRecordList(type);
+		String entity = (String) response.getEntity();
+		statusType = response.getStatusInfo();
+		return entity;
+	}
+
+	public String testCreateRecord() {
+		UriInfo uriInfo = new TestUri();
+		SpiderInstanceProvider.setSpiderDependencyProvider(
+				DependencyProviderForMultipleTestsWorkingTogether.spiderDependencyProvider);
+		RecordEndpoint recordEndpoint = new RecordEndpoint(uriInfo);
+		Response response = recordEndpoint.createRecord(type, json);
 		String entity = (String) response.getEntity();
 		statusType = response.getStatusInfo();
 		return entity;
