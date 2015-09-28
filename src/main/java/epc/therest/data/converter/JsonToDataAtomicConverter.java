af1f9ee63449ca56ece8a505345d01875b1fba8c
Uwe Klosa <uwe.klosa@ub.uu.se>
2015-09-28 17:06:28 +0200

    EPC-104: fixed test failures with nameInData

--- src/main/java/epc/therest/data/converter/JsonToDataAtomicConverter.java
+++ src/main/java/epc/therest/data/converter/JsonToDataAtomicConverter.java
@@ -7,53 +7,62 @@ import epc.therest.json.parser.JsonParseException;
 import epc.therest.json.parser.JsonString;
 import epc.therest.json.parser.JsonValue;
 
-public final class JsonToDataAtomicConverter implements JsonToDataConverter {
-	private JsonObject jsonObject;
+public final class JsonToDataAtomicConverter implements JsonToDataConverter
+{
+    private JsonObject jsonObject;
 
-	static JsonToDataAtomicConverter forJsonObject(JsonObject jsonObject) {
-		return new JsonToDataAtomicConverter(jsonObject);
-	}
+    static JsonToDataAtomicConverter forJsonObject(JsonObject jsonObject)
+    {
+        return new JsonToDataAtomicConverter(jsonObject);
+    }
 
-	private JsonToDataAtomicConverter(JsonObject jsonObject) {
-		this.jsonObject = jsonObject;
-	}
+    private JsonToDataAtomicConverter(JsonObject jsonObject)
+    {
+        this.jsonObject = jsonObject;
+    }
 
-	@Override
-	public RestDataElement toInstance() {
-		try {
-			return tryToInstantiate();
-		} catch (Exception e) {
-			throw new JsonParseException("Error parsing jsonObject: "+e.getMessage(), e);
-		}
-	}
+    @Override
+    public RestDataElement toInstance()
+    {
+        try {
+            return tryToInstantiate();
+        } catch (Exception e) {
+            throw new JsonParseException("Error parsing jsonObject: " + e.getMessage(), e);
+        }
+    }
 
-	private RestDataElement tryToInstantiate() {
-		validateJsonData();
-		String nameInData = getNameInDataFromJsonObject();
-		JsonString value = (JsonString) jsonObject.getValue(nameInData);
-		return RestDataAtomic.withNameInDataAndValue(nameInData, value.getStringValue());
-	}
+    private RestDataElement tryToInstantiate()
+    {
+        validateJsonData();
+        String nameInData = getNameInDataFromJsonObject();
+        JsonString value = (JsonString) jsonObject.getValue(nameInData);
+        return RestDataAtomic.withNameInDataAndValue(nameInData, value.getStringValue());
+    }
 
-	private String getNameInDataFromJsonObject() {
-		return jsonObject.keySet().iterator().next();
-	}
+    private String getNameInDataFromJsonObject()
+    {
+        return jsonObject.keySet().iterator().next();
+    }
 
-	private void validateJsonData() {
-		validateOnlyOneKeyValuePairAtTopLevel();
-		validateNameInDataValueIsString();
-	}
+    private void validateJsonData()
+    {
+        validateOnlyOneKeyValuePairAtTopLevel();
+        validateNameInDataValueIsString();
+    }
 
-	private void validateOnlyOneKeyValuePairAtTopLevel() {
-		if (jsonObject.size() != 1) {
-			throw new JsonParseException("Atomic data can only contain one key value pair");
-		}
-	}
+    private void validateOnlyOneKeyValuePairAtTopLevel()
+    {
+        if (jsonObject.size() != 1) {
+            throw new JsonParseException("Atomic data can only contain one key value pair");
+        }
+    }
 
-	private void validateNameInDataValueIsString() {
-		String nameInData = getNameInDataFromJsonObject();
-		JsonValue value = jsonObject.getValue(nameInData);
-		if(!(value instanceof JsonString)){
-			throw new JsonParseException("Value of atomic data must be a String");
-		}
-	}
+    private void validateNameInDataValueIsString()
+    {
+        String nameInData = getNameInDataFromJsonObject();
+        JsonValue value = jsonObject.getValue(nameInData);
+        if (!(value instanceof JsonString)) {
+            throw new JsonParseException("Value of atomic data \"" + nameInData + "\" must be a String");
+        }
+    }
 }
