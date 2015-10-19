64ab77ebc72502c953edaa2ad2da6d5b99284377
madeleine <madeleine.kennback@ub.uu.se>
2015-10-19 12:29:04 +0200

    CORA-94, added read incoming links

--- src/test/java/se/uu/ub/cora/therest/data/converter/spider/ActionSpiderToRestConverterTest.java
+++ src/test/java/se/uu/ub/cora/therest/data/converter/spider/ActionSpiderToRestConverterTest.java
@@ -1,16 +1,15 @@
 package se.uu.ub.cora.therest.data.converter.spider;
 
-import static org.testng.Assert.assertEquals;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import se.uu.ub.cora.spider.data.Action;
+import se.uu.ub.cora.therest.data.ActionLink;
 
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import se.uu.ub.cora.spider.data.Action;
-import se.uu.ub.cora.therest.data.ActionLink;
+import static org.testng.Assert.assertEquals;
 
 public class ActionSpiderToRestConverterTest {
 	private String baseURL = "http://localhost:8080/therest/rest/record/";
@@ -35,6 +34,21 @@ public class ActionSpiderToRestConverterTest {
 		assertEquals(actionLink.getAction(), Action.READ);
 		assertEquals(actionLink.getURL(),
 				"http://localhost:8080/therest/rest/record/recordType/recordId");
+		assertEquals(actionLink.getRequestMethod(), "GET");
+	}
+
+	@Test
+	public void testToRestReadIncomingList(){
+		Action action = Action.READ_INCOMING_LINKS;
+		actions.add(action);
+		ActionSpiderToRestConverter actionSpiderToRestConverter = ActionSpiderToRestConverter
+				.fromSpiderActionsWithBaseURLAndRecordTypeAndRecordId(actions, baseURL,
+						"recordType", "recordId");
+		Map<String, ActionLink> actionLinks = actionSpiderToRestConverter.toRest();
+		ActionLink actionLink = actionLinks.get("read_incoming_links");
+		assertEquals(actionLink.getAction(), Action.READ_INCOMING_LINKS);
+		assertEquals(actionLink.getURL(),
+				"http://localhost:8080/therest/rest/record/recordType/recordId/incomingLinks");
 		assertEquals(actionLink.getRequestMethod(), "GET");
 	}
 
@@ -71,4 +85,32 @@ public class ActionSpiderToRestConverterTest {
 		assertEquals(actionLink.getRequestMethod(), "DELETE");
 	}
 
+	@Test
+	public void testToRestURLsWithAllActions(){
+		actions.add(Action.READ_INCOMING_LINKS);
+		actions.add(Action.READ);
+		actions.add(Action.DELETE);
+		actions.add(Action.UPDATE);
+
+		ActionSpiderToRestConverter actionSpiderToRestConverter = ActionSpiderToRestConverter
+				.fromSpiderActionsWithBaseURLAndRecordTypeAndRecordId(actions, baseURL,
+						"recordType", "recordId");
+		Map<String, ActionLink> actionLinks = actionSpiderToRestConverter.toRest();
+
+		ActionLink delete = actionLinks.get("delete");
+		assertEquals(delete.getURL(),
+				"http://localhost:8080/therest/rest/record/recordType/recordId");
+
+		ActionLink readIncomingLinks = actionLinks.get("read_incoming_links");
+		assertEquals(readIncomingLinks.getURL(),
+				"http://localhost:8080/therest/rest/record/recordType/recordId/incomingLinks");
+
+		ActionLink update = actionLinks.get("update");
+		assertEquals(update.getURL(),
+				"http://localhost:8080/therest/rest/record/recordType/recordId");
+
+		ActionLink read = actionLinks.get("read");
+		assertEquals(read.getURL(),
+				"http://localhost:8080/therest/rest/record/recordType/recordId");
+	}
 }
