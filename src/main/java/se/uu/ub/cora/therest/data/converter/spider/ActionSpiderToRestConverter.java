1593fb4e9b73cc2ede8993a38449e27e1b08f5e9
Uwe Klosa <uwe.klosa@ub.uu.se>
2015-10-20 10:41:23 +0200

    CORA-95: added GPLv3 license to java files and pom.xml

--- src/main/java/se/uu/ub/cora/therest/data/converter/spider/ActionSpiderToRestConverter.java
+++ src/main/java/se/uu/ub/cora/therest/data/converter/spider/ActionSpiderToRestConverter.java
@@ -1,11 +1,30 @@
-package se.uu.ub.cora.therest.data.converter.spider;
+/*
+ * Copyright 2015 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import se.uu.ub.cora.spider.data.Action;
-import se.uu.ub.cora.therest.data.ActionLink;
+package se.uu.ub.cora.therest.data.converter.spider;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+
+import se.uu.ub.cora.spider.data.Action;
+import se.uu.ub.cora.therest.data.ActionLink;
 
 public final class ActionSpiderToRestConverter {
 
