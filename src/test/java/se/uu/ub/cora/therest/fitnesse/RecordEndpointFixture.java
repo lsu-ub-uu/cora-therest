5ce36245a08c2ac0e49d670418346d8d1a41a2f4
madeleine <madeleine.kennback@ub.uu.se>
2016-04-01 12:30:51 +0200

    merged master into maddekenn_master

--- src/test/java/se/uu/ub/cora/therest/fitnesse/RecordEndpointFixture.java
+++ src/test/java/se/uu/ub/cora/therest/fitnesse/RecordEndpointFixture.java
@@ -19,14 +19,14 @@
 
 package se.uu.ub.cora.therest.fitnesse;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.StatusType;
-import javax.ws.rs.core.UriInfo;
-
 import se.uu.ub.cora.spider.dependency.SpiderInstanceProvider;
 import se.uu.ub.cora.systemone.SystemOneDependencyProvider;
 import se.uu.ub.cora.therest.record.RecordEndpoint;
 import se.uu.ub.cora.therest.record.TestUri;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.StatusType;
+import javax.ws.rs.core.UriInfo;
 
 public class RecordEndpointFixture {
 	private String id;
