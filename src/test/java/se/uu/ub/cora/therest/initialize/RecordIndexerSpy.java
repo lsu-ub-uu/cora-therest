98e601ea5db1966102e9429e4a2e608a61540f31
olov <olov@c88ac58041ae>
2017-11-15 13:41:29 +0000

    CORA-450 possible abstractRecordType_recordId sent to indexer

--- src/test/java/se/uu/ub/cora/therest/initialize/RecordIndexerSpy.java
+++ src/test/java/se/uu/ub/cora/therest/initialize/RecordIndexerSpy.java
@@ -1,12 +1,14 @@
 package se.uu.ub.cora.therest.initialize;
 
+import java.util.List;
+
 import se.uu.ub.cora.bookkeeper.data.DataGroup;
 import se.uu.ub.cora.spider.search.RecordIndexer;
 
 public class RecordIndexerSpy implements RecordIndexer {
 
 	@Override
-	public void indexData(DataGroup recordIndexData, DataGroup record) {
+	public void indexData(List<String> ids, DataGroup recordIndexData, DataGroup record) {
 	}
 
 	@Override
