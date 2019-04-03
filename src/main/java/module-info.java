ad35ae918559c77e1334bdabd546732577ada5db
olovm <olov.mckie@ub.uu.se>
2019-04-03 09:14:19 +0000

    updating for use of java modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module se.uu.ub.cora.therest {
+	requires se.uu.ub.cora.spider;
+	requires java.activation;
+	requires java.ws.rs;
+	requires javax.servlet.api;
+	requires jersey.media.multipart;
+}
