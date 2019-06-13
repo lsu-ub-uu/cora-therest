0676475ef7c2d76c8728d3ad8f239b2d94229186
olovm <olov.mckie@ub.uu.se>
2019-06-13 08:52:28 +0000

    changing theRest to be a jar instead of a war

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,20 @@
 module se.uu.ub.cora.therest {
-	requires se.uu.ub.cora.logger;
-	requires se.uu.ub.cora.storage;
-	requires se.uu.ub.cora.spider;
-	requires java.activation;
-	requires java.ws.rs;
-	requires javax.servlet.api;
-	requires jersey.media.multipart;
-	requires se.uu.ub.cora.httphandler;
-	requires se.uu.ub.cora.metacreator;
-	requires se.uu.ub.cora.gatekeeperclient;
-	requires se.uu.ub.cora.solrsearch;
+	requires transitive se.uu.ub.cora.logger;
+	requires transitive se.uu.ub.cora.storage;
+	requires transitive se.uu.ub.cora.spider;
+	requires transitive java.activation;
+	requires transitive java.ws.rs;
+	requires transitive javax.servlet.api;
+	requires transitive jersey.media.multipart;
+	requires transitive se.uu.ub.cora.httphandler;
+	requires transitive se.uu.ub.cora.metacreator;
+	requires transitive se.uu.ub.cora.gatekeeperclient;
+	requires transitive se.uu.ub.cora.solrsearch;
 
 	uses se.uu.ub.cora.storage.RecordStorageProvider;
 	uses se.uu.ub.cora.storage.StreamStorageProvider;
 	uses se.uu.ub.cora.storage.RecordIdGeneratorProvider;
 	uses se.uu.ub.cora.storage.MetadataStorageProvider;
+
+	exports se.uu.ub.cora.therest.initialize;
 }
