6c474165cea049b42dac515ef968076a41dab46a
maddekenn <madeleine.kennback@ub.uu.se>
2019-11-25 12:29:21 +0000

    New versions of dependencies

--- src/test/java/se/uu/ub/cora/therest/data/DataGroupSpy.java
+++ src/test/java/se/uu/ub/cora/therest/data/DataGroupSpy.java
@@ -20,11 +20,13 @@
 package se.uu.ub.cora.therest.data;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import se.uu.ub.cora.data.DataAtomic;
+import se.uu.ub.cora.data.DataAttribute;
 import se.uu.ub.cora.data.DataElement;
 import se.uu.ub.cora.data.DataGroup;
 
@@ -151,6 +153,13 @@ public class DataGroupSpy implements DataGroup {
 
 	@Override
 	public Map<String, String> getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> getAllGroupsWithNameInDataAndAttributes(String childNameInData,
+			DataAttribute... childAttributes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
