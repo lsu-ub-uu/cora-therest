e547feb8197a0424422b6021d6ff68d4ae3cfc2b
madde <madde@0041aac71c44>
2021-05-11 11:14:00 +0000

    Implementing new methods from bookkeeper in spies

--- src/test/java/se/uu/ub/cora/therest/record/DataValidatorAlwaysInvalidSpy.java
+++ src/test/java/se/uu/ub/cora/therest/record/DataValidatorAlwaysInvalidSpy.java
@@ -40,4 +40,10 @@ public class DataValidatorAlwaysInvalidSpy implements DataValidator {
 		return null;
 	}
 
+	@Override
+	public ValidationAnswer validateIndexSettings(String recordType, DataGroup indexSettings) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
