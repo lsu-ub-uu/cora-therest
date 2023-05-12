98e248d0544b204b4ca3eabf3d2e60f3e15360c7
pere <pere@5ff3e0758b40>
2023-05-12 13:26:13 +0000

    update dependencies

--- src/test/java/se/uu/ub/cora/therest/coradata/DataAtomicSpy.java
+++ src/test/java/se/uu/ub/cora/therest/coradata/DataAtomicSpy.java
@@ -19,6 +19,7 @@
 package se.uu.ub.cora.therest.coradata;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import se.uu.ub.cora.data.DataAtomic;
 import se.uu.ub.cora.data.DataAttribute;
@@ -84,6 +85,12 @@ public class DataAtomicSpy implements DataAtomic {
 	public Collection<DataAttribute> getAttributes() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Optional<String> getAttributeValue(String nameInData) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
 	}
 
 }
