6a534d1c4f941a183a08d811b3f690d468c6848e
perebartrolisimo <pere.bartroli@ub.uu.se>
2023-09-27 14:10:52 +0000

    CORA-2265 (CORA-2266) adding contentanalyzer

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module se.uu.ub.cora.therest {
 	requires se.uu.ub.cora.data;
 	requires se.uu.ub.cora.converter;
 	requires se.uu.ub.cora.initialize;
+	requires se.uu.ub.cora.contentanalyzer;
 
 	uses se.uu.ub.cora.storage.RecordStorageProvider;
 	uses se.uu.ub.cora.storage.StreamStorageProvider;
